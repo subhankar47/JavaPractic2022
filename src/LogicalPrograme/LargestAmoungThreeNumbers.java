bda441105abfd99ba8762bcadfddcd7c716751d5
SUBHANKAR <subhankarjena14@gmail.com>
2022-06-27 21:56:50 +0530

    my 1st commite

---
+++ src/LogicalPrograme/LargestAmoungThreeNumbers.java
@@ -0,0 +1,18 @@
+package LogicalPrograme;
+
+public class LargestAmoungThreeNumbers {
+    public static void main(String[] args){
+    int x=600;
+    int y=500;
+    int z=800;
+    if(x>y && x>z){
+        System.out.println("x is the greatest");
+    }
+    else if(y>z){
+        System.out.println("y is the greatest");
+    }
+    else{
+        System.out.println("z is the greatest");
+    }
+    }
+}
