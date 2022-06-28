ba592e8b9babab0466a0d2dcc92ef25c16a31ea8
SUBHANKAR <subhankarjena14@gmail.com>
2022-06-28 18:27:51 +0530

    my 1st commite

---
+++ src/LogicalPrograme/Positive_NegativeNumber.java
@@ -0,0 +1,16 @@
+package LogicalPrograme;
+
+public class Positive_NegativeNumber {
+    public static void main(String[] args){
+    double number=0.1;
+    if(number>0.0){
+        System.out.println(number+"this number is positive ");
+    }
+    else if(number<0.0){
+        System.out.println(number+"this number is negative");
+    }
+    else{
+        System.out.println(number+"is zero");
+    }
+    }
+}
