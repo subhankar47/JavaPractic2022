2be8fc7c835430323bf7fddd658218e4dae0368d
SUBHANKAR <subhankarjena14@gmail.com>
2022-06-29 15:01:38 +0530

    my 1st commite

---
+++ src/LogicalPrograme/ReverseNumber.java
@@ -0,0 +1,15 @@
+package LogicalPrograme;
+
+public class ReverseNumber {
+    public static void main(String[] args){
+        int no=5423;
+        int rem;
+        int rev=0;
+        while(no!=0){
+            rem=no%10;
+            rev=rev*10+rem;
+            no=no/10;
+        }
+        System.out.println(rev);
+    }
+}
