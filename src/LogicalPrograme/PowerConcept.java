1bf2abb1ecc8b5307eb44479c7447cd5c4365ea1
SUBHANKAR <subhankarjena14@gmail.com>
2022-06-30 14:50:14 +0530

    my 1st commite

---
+++ src/LogicalPrograme/PowerConcept.java
@@ -0,0 +1,15 @@
+package LogicalPrograme;
+
+public class PowerConcept {
+    public static void main(String[] args){
+        //3^4= 81
+        int base=3;
+        int exponent=4;
+        long result=1;
+        while(exponent!=0){
+            result*=base;//result*base;
+            --exponent;
+        }
+        System.out.println(result);
+    }
+}
