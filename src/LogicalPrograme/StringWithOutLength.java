b368c6023f149229c0653003d441b29b89db8e14
SUBHANKAR <subhankarjena14@gmail.com>
2022-07-05 23:55:00 +0530

    my 1st commite

---
+++ src/LogicalPrograme/StringWithOutLength.java
@@ -0,0 +1,50 @@
+package LogicalPrograme;
+
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * WAP to find out the length of the String without using length()
+ *
+ * @Subhankar
+ */
+public class StringWithOutLength {
+    public static void main(String[] args) {
+        String s = "Subhankar";
+        System.out.println(s.toCharArray().length);
+        System.out.println(s.lastIndexOf(""));
+        Matcher m = Pattern.compile("$").matcher(s);
+        m.find();
+        int leng = m.end();
+        System.out.println(leng);
+        System.out.println(s.split("").length);
+        //int l = new StringBuilder(s).length();
+        //System.out.println(l);
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            count++;
+        }
+        System.out.println(count);
+        System.out.println(getlength("Subhankar Jena"));
+        int l1 = 0;
+        String str = "Tester and Developer";
+        try {
+            l1 = str.getBytes("UTF-16BE").length / 2;
+        } catch (Exception e) {
+        }
+        System.out.println(l1);
+    }
+
+    public static int getlength(String s) {
+        int i = 0;
+        try {
+            while (true) {
+                s.charAt(i);
+                i++;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            return i;
+        }
+    }
+}
