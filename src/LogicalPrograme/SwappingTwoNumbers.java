2fe35e8bf5b766cf8e51cb47be4b59caad7c0f90
SUBHANKAR <subhankarjena14@gmail.com>
2022-07-05 23:54:19 +0530

    my 1st commite

--- src/LogicalPrograme/SwappingTwoNumbers.java
+++ src/LogicalPrograme/SwappingTwoNumbers.java
@@ -1,6 +1,6 @@
 package LogicalPrograme;
 
-public class Swapping2Numbers {
+public class SwappingTwoNumbers {
     public static void main(String[] args) {
         int a = 10, b = 20;
         System.out.println("Before Swapping Values Are..." + a + " " + b);
