96c2dafd46487b3f8e406d95c575db309463ac09
SUBHANKAR <subhankarjena14@gmail.com>
2022-07-08 23:26:55 +0530

    my 1st commite

--- src/LogicalPrograme/MaxReptCharInStringHasMap.java
+++ src/LogicalPrograme/MaxReptCharInStringHasMap.java
@@ -1,5 +1,29 @@
 package LogicalPrograme;
 
-public class MaxReptCharInStringHasMap {
+import java.util.HashMap;
+import java.util.Map;
 
+public class MaxReptCharInStringHasMap {
+    public static void main(String[] args) {
+        String s = "This is Demos";
+        s = s.replaceAll("\\s", "");
+        HashMap<Character, Integer> hm = new HashMap<>();
+        char[] c = s.toCharArray();
+        for (char ch : c) {
+            if (hm.containsKey(ch)) {
+                hm.put(ch, hm.get(ch) + 1);
+            } else {
+                hm.put(ch, 1);
+            }
+        }
+        int maxCount = 0;
+        char maxChar = ' ';
+        for (Map.Entry<Character, Integer> me : hm.entrySet()) {
+            if (maxCount < me.getValue()) {
+                maxCount = me.getValue();
+                maxChar = me.getKey();
+            }
+        }
+        System.out.println("Maximum Repeated Character Is " + maxChar);
+    }
 }
