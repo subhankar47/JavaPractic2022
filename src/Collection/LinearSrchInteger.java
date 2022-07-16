75ee314ef9daa7aa626bd0a3a82037aeec552110
SUBHANKAR <subhankarjena14@gmail.com>
2022-07-16 19:11:58 +0530

    my 1st commite

--- src/Collection/LinearSrchInteger.java
+++ src/Collection/LinearSrchInteger.java
@@ -4,10 +4,15 @@ public class LinearSrchInteger {
     public static void main(String[] args) {
         int[] a = {5, 3, 6, 1, 7, 2};
         int item = 1;
+        int temp = 0;
         for (int i = 0; i < a.length; i++) {
             if (a[i] == item) {
                 System.out.println("Item is present at " + i + " index Position");
+                temp = temp + 1;
             }
+        }
+        if (temp == 0) {
+            System.out.println("Item is not found in List");
         }
     }
 }
