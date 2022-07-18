1f152da177657da86f25c4117e53698a1f42b59f
SUBHANKAR <subhankarjena14@gmail.com>
2022-07-18 21:32:26 +0530

    my 1st commite

--- src/StringJava/ReadDataFromTextFile.java
+++ src/StringJava/ReadDataFromTextFile.java
@@ -1,19 +1,26 @@
 package StringJava;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class ReadDataFromTextFile {
     public static void main(String[] args) throws IOException {
         //Approach-1 using FileReader BufferReader
-        FileReader fr = new FileReader("D:\\Intellij\\JavaPractic2022\\Test.txt");
+       /* FileReader fr = new FileReader("D:\\Intellij\\JavaPractic2022\\Test.txt");
         BufferedReader br = new BufferedReader(fr);
         String str;
         while ((str = br.readLine()) != null) {
             System.out.println(str);
         }
-        br.close();
+        br.close();*/
+        //Approach-2 using scanner and File
+        File f = new File("D:\\\\Intellij\\\\JavaPractic2022\\\\Test.txt");
+        Scanner sc = new Scanner(f);
+       /* while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }*/
+        //Approach-3
+        sc.useDelimiter("\\z");
+        System.out.println(sc.next());
     }
 }
