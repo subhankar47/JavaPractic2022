182c5b687f23542b8b3966d331d48cd15d7a7374
SUBHANKAR <subhankarjena14@gmail.com>
2022-07-18 20:57:57 +0530

    my 1st commite

--- src/StringJava/ReadDataFromTextFile.java
+++ src/StringJava/ReadDataFromTextFile.java
@@ -1,10 +1,19 @@
 package StringJava;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ReadDataFromTextFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //Approach-1 using FileReader BufferReader
-        FileReader fr = new FileReader("");
+        FileReader fr = new FileReader("D:\\Intellij\\JavaPractic2022\\Test.txt");
+        BufferedReader br = new BufferedReader(fr);
+        String str;
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+        br.close();
     }
 }
