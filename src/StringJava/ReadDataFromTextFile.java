9305cfd423203be35c75fc6e10e9e432e53a918c
SUBHANKAR <subhankarjena14@gmail.com>
2022-07-18 20:34:28 +0530

    my 1st commite

---
+++ src/StringJava/ReadDataFromTextFile.java
@@ -0,0 +1,10 @@
+package StringJava;
+
+import java.io.FileReader;
+
+public class ReadDataFromTextFile {
+    public static void main(String[] args) {
+        //Approach-1 using FileReader BufferReader
+        FileReader fr = new FileReader("");
+    }
+}
